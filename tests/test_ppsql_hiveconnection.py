a8362279fa4ed4a1a21e84ae0f627ed3d9a6d008
pauljv92 <pvijaya2@illinois.edu>
2018-09-04 17:36:48 -0700

    Update test_ppsql_hiveconnection.py

--- tests/test_ppsql_hiveconnection.py
+++ tests/test_ppsql_hiveconnection.py
@@ -3,7 +3,7 @@ from thriftpy.transport import TTransportException
 import unittest
 
 class TestPPSqlHiveConnection(unittest.TestCase):
-    
+
     def test_invalid_hive_connection(self):
         with self.assertRaises(TTransportException):
             hc = HiveConnection("fake", "fake", 1234, "GSSAPI", "fake")
